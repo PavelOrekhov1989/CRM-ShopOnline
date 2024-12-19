d4aff9400a523de30f126441a2733bde75304717
Павел Орехов <or1989@list.ru>
2024-12-19 22:24:05 +0300

    modul4less1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const product_name = 'кастрюля';
-const kollichestvo_tovara = '5';
+const kollichestvo_tovara = 5;
 const category = 'посуда';
-const price = '800';
+const price = 800;
 
-console.log(product_name);
+const sum = (kollichestvo_tovara * price);
+
+console.log(sum);
